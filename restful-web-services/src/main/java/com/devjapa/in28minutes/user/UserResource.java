6e0bed970e88f0514dadec5dcae2e45909d54608
Leandro Almeida <leandrotrovilho@gmail.com>
2020-11-30 16:49:44 -0300

    Add ResponseEntityExceptionHandler

--- restful-web-services/src/main/java/com/devjapa/in28minutes/user/UserResource.java
+++ restful-web-services/src/main/java/com/devjapa/in28minutes/user/UserResource.java
@@ -27,7 +27,10 @@ public class UserResource {
 	
 	@GetMapping("/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user == null)
+			throw new UserNotFoundException("id-"+ id);
+		return user;
 	}
 	
 	// input - details of user

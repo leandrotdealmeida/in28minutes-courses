0d7e6e98b4c14d83634396eab3a6fb6dc7174f35
Leandro Almeida <leandrotrovilho@gmail.com>
2020-12-02 17:52:35 -0300

    Delete User

---
+++ restful-web-services/src/main/java/com/devjapa/in28minutes/user/Post.java
@@ -0,0 +1,31 @@
+package com.devjapa.in28minutes.user;
+
+public class Post {
+
+	private Integer id;
+
+	private String title;
+
+	public Post(Integer id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}	
+
+}

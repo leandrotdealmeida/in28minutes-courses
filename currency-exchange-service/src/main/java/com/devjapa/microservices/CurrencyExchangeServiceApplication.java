7234d814e894f69a8bb19f3aefbb6713408d9628
devjapa <leandrotrovilho@gmail.com>
2021-08-04 09:27:47 -0300

    feat(Jpa): Add Jpa

---
+++ currency-exchange-service/src/main/java/com/devjapa/microservices/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.devjapa.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
